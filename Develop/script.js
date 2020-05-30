71326080ebe00f466e3eb42446731a2b5239ade3
Luke Evans <lukeevanstax@gmail.com>
2020-05-30 18:36:06 -0400

    updating JS file to include lowercase, uppercase, specialCharacters, numericalCharacters, and beginning stages of generate password function

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,28 @@
 // Assignment Code
 const generateBtn = document.querySelector("#generate");
 
+const lowercase = "abcedfghijklmnopqrstuvwxyz";
+const uppercase = "ABCEDFGHIJKLMNOPQRSTUVWXYZ";
+const specialCharacters = "@#$%^&*=|";
+const numericalCharacters = "1234567890";
+
+let passwordCriteria = 8 - 128;
+
+const passwordCriteria = prompt("How long would you like your password to be? Choose between 8-128 characters");
+alert("You want " + passwordCriteria + " characters");
+
+function generatePassword()
+const uppercaseConfirm = confirm("does the password need to contain random upper case letters?")
+
+const lowercaseConfirm = confirm("would you like the PW need to contain random lowercase letters?")
+
+const specialCharacters = confirm("does the PW need to contain special random characters?")
+
+const numericalCharacters = confirm("would you like the PW to contain random numbers?")
+
+
+
+
 // Write password to the #password input
 function writePassword() {
     const password = generatePassword();
